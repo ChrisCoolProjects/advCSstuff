330d65f144063c3ecea65f6c7317e677109768ae
ChrisCoolProjects <32934802+ChrisCoolProjects@users.noreply.github.com>
2017-11-03 09:43:46 -0500

    Update SalesDatapt2.cpp

--- Salesdatap2/SalesDatapt2.cpp
+++ Salesdatap2/SalesDatapt2.cpp
@@ -16,16 +16,22 @@ class SALESREC
 };
 void simpleSortTotal(SALESREC* s[] , int c)
 {
-   SALESREC* temp;
 	for (int i=0; i < c; i++)
-		for (int j=i; j < c; j++)
-		if (s[i]->Total < s[j]->Total)
+	{
+	for (int j=i; j < c; j++)
 		{
-         temp = s[i];
-         s[i] = s[j];
-		   s[j] = temp;
-      }
-} 
+		cout << j << " The code is in the sort method" << endl;	
+		if (s[j]->Total < s[i]->Total)
+			{
+			SALESREC* temp = new SALESREC[c];
+			temp = s[i];
+			s[i] = s[j];
+			s[j] = temp;
+			delete[] temp;
+			}
+		}
+	}
+}
 //the blank should be a pointer array
 void simpleSortTotal(SALESREC* s[], int c);
 
@@ -33,32 +39,31 @@ int main(){
 	ifstream infile;
 	int i=0;
 	char cNum[10] ;
-	class SALESREC salesArr[40];
+	SALESREC* salesArr[40];
 	int   salesArrayCount;
-   float un;
+    float un;
 	SALESREC* s[40];
-   cout << "This is working for now" << endl;
 	infile.open("SalesDataP2.csv", ifstream::in);
-   cout << "This is still working for now" << endl;
+	cout << "The csv is being opened" << endl;
 
 if (infile.is_open())
 {
 int   c=0;
    while (infile.good())
 	{
-		infile.getline(salesArr[c].date, 256, ',');
-		infile.getline(salesArr[c].region, 256, ',');
-		infile.getline(salesArr[c].rep, 256, ',');
-		infile.getline(salesArr[c].item, 256, ',');
-		infile.getline(cNum, 256, ',');
-		salesArr[c].units = atoi(cNum);
-      un = atof(cNum);
-		infile.getline(cNum, 256, '\n');
-		salesArr[c].unitCost = atof(cNum);
-		salesArr[c].Total = atof(cNum);
-      salesArr[c].Total = salesArr[c].unitCost * un;
-		i++;
-		c++;
+	infile.getline(salesArr[c]->date, 256, ',');
+	infile.getline(salesArr[c]->region, 256, ',');
+	infile.getline(salesArr[c]->rep, 256, ',');
+	infile.getline(salesArr[c]->item, 256, ',');
+	infile.getline(cNum, 256, ',');
+	salesArr[c]->units = atoi(cNum);
+	un = atof(cNum);
+	infile.getline(cNum, 256, '\n');
+	salesArr[c]->unitCost = atof(cNum);
+	salesArr[c]->Total = atof(cNum);
+	salesArr[c]->Total = salesArr[c]->unitCost * un;
+	i++;
+	c++;
 	}
 salesArrayCount = c-1;
 infile.close();
@@ -67,19 +72,21 @@ else
 	{
 	cout << "Error opening file";
 	}
-	   cout << "This is working for now" << endl;
 
 	// Initialize the sort array pointers
 for (int i=0; i < salesArrayCount; i++)
 {
-		s[i] = &salesArr[i];
-		simpleSortTotal (s, salesArrayCount);
+	
+
+		simpleSortTotal (salesArr, salesArrayCount);
+		cout << i << "This is after the sort method is called." << endl;
+
 }
    for (int i=0; i < salesArrayCount; i++)
    {
 		cout << "Record: " << s[i]->date <<", " << s[i]->region << ", " << s[i]->rep << ", " << s[i]->item << ", " << s[i]->units << ", " << s[i]->unitCost << ", " << s[i]->Total << endl;
    }
-      cout << "This is working for now" << endl;
+   cout << "This is working for now" << endl;
 
 }
  
